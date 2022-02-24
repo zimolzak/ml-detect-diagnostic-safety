a10d5a5a3f42380292dcc73827a1b80f9eb526c2
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-02-24 04:22:47 -0600

    Slightly better test.

--- test_umap_helpers.py
+++ test_umap_helpers.py
@@ -2,11 +2,12 @@ import numpy as np
 from umap_helpers import points_near
 
 MY_ARY = np.array([[4, 4], [4, 5], [4, 6], [5, 4], [5, 5], [5, 6], [6, 4], [6, 5], [6, 6], [4.5, 4.5]])
+#                    0       1       2       3       4
 
 
 def test_points_near():
     my_k = 4
-    p = points_near(5, 5, MY_ARY, k=my_k)
-    n_points_returned = p.shape[0]
+    my_indices = points_near(5, 5, MY_ARY, k=my_k)
+    n_points_returned = my_indices.shape[0]
     assert n_points_returned <= my_k
-    print("\n", p)
+    assert my_indices[0] == 4  # Point 4 in MY_ARY is [5,5] so distance 0.
