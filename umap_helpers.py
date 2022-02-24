e48d21bc23117b0f743d2e6ef5ce901093d5d6cb
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-02-24 04:17:17 -0600

    Improve docstring

--- umap_helpers.py
+++ umap_helpers.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 
-def points_near(x0, y0, point_array, k=10):
-    """Take a numpy array (n * 2) of ordered pairs. Figure out which are closest to specified x, y.
+def points_near(x0: float, y0: float, point_array: np.ndarray, k: int = 10) -> np.ndarray:
+    """Take a numpy array (n * 2) of ordered pairs. Figure out which are closest to specified x0, y0.
+    Note: Returns only the indices (not the points themselves, not the distances).
 
-    :param x0: x-coordinate of center of neighborhood to select
-    :param y0: y-coordinate of center of neighborhood to select
-    :param point_array: Numpy array of points
+    :param x0: x-coordinate of center of neighborhood to select.
+    :param y0: y-coordinate of center of neighborhood to select.
+    :param point_array: Numpy array of points (x is column 0, y is column 1).
     :param k: How many nearest points we want.
+    :return: Array of indices of the nearest points.
     """
 
     n_total_points = point_array.shape[0]
