7bc1f485eb5cf279290a10520c03e8019aa8df3b
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-02-24 06:20:38 -0600

    Refactor to support >2-D points. Test shape exception (not implemented yet).

--- umap_helpers.py
+++ umap_helpers.py
@@ -1,18 +1,22 @@
 import numpy as np
 
 
-def points_near(x0: float, y0: float, point_array: np.ndarray, k: int = 10) -> np.ndarray:
-    """Take a numpy array (n * 2) of ordered pairs. Figure out which are closest to specified x0, y0.
+def points_near(point_of_interest: np.ndarray, point_array: np.ndarray, k: int = 10) -> np.ndarray:
+    """Take a numpy array representing N points. Figure out which are closest to a single specified point.
     Note: Returns only the indices (not the points themselves, not the distances).
 
-    :param x0: x-coordinate of center of neighborhood to select.
-    :param y0: y-coordinate of center of neighborhood to select.
+    Usually point_array would be shape (N, 2) and point_of_interest would be shape (2), representing points in 2-D
+    plane. But should work dimensions other than 2 (any number of matrix columns).
+
+    :param point_of_interest: Numpy array to specify a single point (center of neighborhood to select).
     :param point_array: Numpy array of points (x is column 0, y is column 1).
     :param k: How many nearest points we want.
     :return: Array of indices of the nearest points.
     """
 
-    x0y0 = np.broadcast_to(np.array([x0, y0]), point_array.shape)
-    distances = np.linalg.norm(x0y0 - point_array, axis=1)
+    # fixme - should probably check assumptions about .shape and .ndim of inputs.
+
+    poi_repeated = np.broadcast_to(point_of_interest, point_array.shape)
+    distances = np.linalg.norm(poi_repeated - point_array, axis=1)
     nearest_indices = distances.argsort()
     return nearest_indices[:k]
