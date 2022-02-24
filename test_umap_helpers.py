1c745f15d73b46ea388c2ac3b527dc55763ac093
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-02-24 05:45:41 -0600

    Loop lots of tests.

--- test_umap_helpers.py
+++ test_umap_helpers.py
@@ -1,13 +1,19 @@
 import numpy as np
 from umap_helpers import points_near
 
-MY_ARY = np.array([[4, 4], [4, 5], [4, 6], [5, 4], [5, 5], [5, 6], [6, 4], [6, 5], [6, 6], [4.5, 4.5]])
-#                    0       1       2       3       4
+EXAMPLE_POINTS = np.array([[4, 4], [4, 5], [4, 6], [5, 4], [5, 5], [5, 6], [6, 4], [6, 5], [6, 6], [4.5, 4.5]])
+#                    0       1       2       3       4                               8         9
 
 
 def test_points_near():
-    my_k = 4
-    my_indices = points_near(5, 5, MY_ARY, k=my_k)
-    n_points_returned = my_indices.shape[0]
-    assert n_points_returned <= my_k
-    assert my_indices[0] == 4  # Point 4 in MY_ARY is [5,5] so distance 0.
+    indices = points_near(5, 5, EXAMPLE_POINTS, 5)
+    assert indices[0] == 4  # Point 4 is [5,5] so distance 0.
+    indices = points_near(4.6, 4.6, EXAMPLE_POINTS)
+    assert indices[0] == 9
+
+    for my_k in range(100):
+        indices = points_near(-7.5, -10.2, EXAMPLE_POINTS, k=my_k)
+        n_points_returned = indices.shape[0]
+        assert n_points_returned <= my_k
+        if my_k > 0:
+            assert indices[0] == 0  # Point 0 is [4,4] so closest to -7.5, -10.2
