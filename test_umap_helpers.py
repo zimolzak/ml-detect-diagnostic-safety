7b24ebd2ba0c14fe3eba4646a23c2449659092f7
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-02-24 03:52:14 -0600

    Fill out function w/ code to select points.

--- test_umap_helpers.py
+++ test_umap_helpers.py
@@ -3,6 +3,9 @@ from umap_helpers import points_near
 
 
 def test_points_near():
+    my_k = 4
     my_ary = np.array([[4, 4], [4, 5], [4, 6], [5, 4], [5, 5], [5, 6], [6, 4], [6, 5], [6, 6], [4.5, 4.5]])
-    print()
-    points_near(5, 5, my_ary)
+    p = points_near(5, 5, my_ary, k=my_k)
+    n_points_returned = p.shape[0]
+    assert n_points_returned <= my_k
+    print("\n", p)
--- umap_helpers.py
+++ umap_helpers.py
@@ -2,8 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-def points_near(x, y, point_array, n_points=10):
-    df = pd.DataFrame(point_array).rename(
-        columns={0: "x", 1: "y"}
-    )
-    print(df)
+def points_near(x0, y0, point_array, k=10):
+    """Take a numpy array (n * 2) of ordered pairs. Figure out which are closest to specified x, y.
+
+    :param x0: x-coordinate of center of neighborhood to select
+    :param y0: y-coordinate of center of neighborhood to select
+    :param point_array: Numpy array of points
+    :param k: How many nearest points we want.
+    """
+
+    n_total_points = point_array.shape[0]
+    x0y0 = np.tile(np.array([x0, y0]), [n_total_points, 1])
+    dists = np.linalg.norm(x0y0 - point_array, axis=1, keepdims=True)
+
+    df = pd.DataFrame(point_array).rename(columns={0: "x", 1: "y"})
+    df["distance"] = dists
+    k_nearest = df.sort_values(by="distance")[:k]
+    return k_nearest
