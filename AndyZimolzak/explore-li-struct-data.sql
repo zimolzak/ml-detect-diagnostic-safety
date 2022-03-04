4ad4de257fa273243f1d0107db1060c1e525d1f2
Zimolzak <Andrew.Zimolzak@va.gov>
2022-03-04 12:45:13 -0600

    AndyZimolzak folder. I inspected all txt and sql.

---
+++ AndyZimolzak/explore-li-struct-data.sql
@@ -0,0 +1,25 @@
+-- rb03 !!!
+
+use [ORD_Singh_201911038D];
+
+select top 17 * from dflt.TableOfInterest
+
+-- spadeRF
+
+select top 17 * from [Dflt].[SpadeRF_030_06_ComorbidIndex]
+select top 17 * from [Dflt].[SpadeRF_030_09_final]
+
+-- 2016 17
+
+select top 17 * from [Dflt].[SpadeRF_Y201617_20210818_60_10_RxOutpatFill]
+select top 17 * from [Dflt].[SpadeRF_Y201617_20211209_030_02_InpatStrokeWithPrior30DayE_Outpat_OnlyDizzHeadache_sentHome]
+
+-- 2016 19
+
+select top 17 * from [Dflt].[SpadeRFAndSpade_Y201619_12_ToFlatCSV]
+
+-- 50 60 70 80 BigSet
+
+select top 17 * from [Dflt].[SpadeRFAndSpade_Y201619_70_BigSet_ICD_2_ExcDx_4_ALLDx_ICD9ICD10]
+select top 17 * from [Dflt].[SpadeRFAndSpade_Y201619_80_BigSet_Lab_01_01_Labchem]
+select top 17 * from [Dflt].[SpadeRFAndSpade_Y201619_80_BigSet_Rx_02_RxOutPatFill]
