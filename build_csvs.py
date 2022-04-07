eb4f280383a3dc2e49e66a7aae557ac9128508ae
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-04-07 12:03:15 -0500

    Refactor better var names and CSV header names.

--- build_csvs.py
+++ build_csvs.py
@@ -11,26 +11,27 @@ icd9-stroke-risk.csv
 icd9-stroke.csv
 """
 
-FILENAMES = LS.split()
+FILE_LIST = LS.split()
 
 if __name__ == '__main__':
-    for name in FILENAMES:
-        df = pd.read_csv(name)
+    for filename in FILE_LIST:
+        df = pd.read_csv(filename)
 
         # Figure out which is our 2nd column (column containing the name of the top-level grouping of ICD codes).
-        if '10' in name:
-            if 'risk' in name:
-                super_column = 'Risk factors description'
+        if '10' in filename:
+            if 'risk' in filename:
+                super_column_name = 'Risk factors description'
             else:
-                super_column = 'ICD10 description'
-            df['icd-repaired'] = df['ICD-10-CM Code'].apply(repair_icd)
+                super_column_name = 'ICD10 description'
+            df['icd'] = df['ICD-10-CM Code'].apply(repair_icd)
 
         else:  # assume ICD-9
-            if 'risk' in name:
-                super_column = 'Description'
+            if 'risk' in filename:
+                super_column_name = 'Description'
             else:
-                super_column = "'ICD-9-CM CODE DESCRIPTION'"  # Yes, it really has single quotes in its name.
-            df['icd-repaired'] = df['ICD-9-CM CODE'].apply(repair_icd)
+                super_column_name = "'ICD-9-CM CODE DESCRIPTION'"  # Yes, it really has single quotes in its name.
+            df['icd'] = df['ICD-9-CM CODE'].apply(repair_icd)
 
-        df_out = df[['icd-repaired', super_column]]
-        df_out.to_csv('tidy-' + name)
+        df['group'] = df[super_column_name]
+        df_out = df[['icd', 'group']]
+        df_out.to_csv('tidy-' + filename)
